096e310b94e34a11cffdffb78adbd1603bbbd31d
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-16 11:39:55 +0700

    implements main page

--- lib/view/MainPage.dart
+++ lib/view/MainPage.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:kdygd/Navigate.dart';
+import 'package:kdygd/model/Topic.dart';
+import 'package:kdygd/view/account/AccountPage.dart';
+import 'package:kdygd/view/common/AccountIcon.dart';
+import 'package:kdygd/view/exam/ExamPage.dart';
+import 'package:kdygd/view/learn/LearnPage.dart';
+
+const _ICON_SIZE = 35.0;
+
+enum MainPageContent {
+	LEARNING,
+	EXAM,
+	ACCOUNT
+}
 
 class MainPage extends StatefulWidget {
 	@override
@@ -6,10 +20,48 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+	MainPageContent _current_content = MainPageContent.LEARNING;
+	Topic _topic;
 	@override
 	Widget build(BuildContext context) => new Scaffold(
-		body: new Center(
-			child: new Text("Main Page"),
+		body: _getBody(),
+		bottomNavigationBar: new BottomNavigationBar(
+			onTap: ( final int new_index ) => setState( () => _current_content = MainPageContent.values[new_index] ),
+			items: <BottomNavigationBarItem>[
+				new BottomNavigationBarItem(
+					icon: Image.asset("images/learn.png", height: _ICON_SIZE, width: _ICON_SIZE,),
+					title: Text('Learn'),
+				),
+				new BottomNavigationBarItem(
+					icon: Image.asset("images/exam.png", height: _ICON_SIZE, width: _ICON_SIZE,),
+					title: Text('Exam'),
+				),
+				new BottomNavigationBarItem(
+					icon: AccountIcon( size: _ICON_SIZE, ),
+					title: Text('Account'),
+				),
+			],
 		),
 	);
+	
+	Widget _getBody() {
+		switch( _current_content ) {
+			case MainPageContent.LEARNING: return LearnPage( topic: _topic, );
+			case MainPageContent.EXAM: return ExamPage( topic: _topic, );
+			case MainPageContent.ACCOUNT: return new AccountPage();
+		}
+	}
+	
+	@override
+	void initState() {
+		super.initState();
+		new Future.delayed(Duration.zero, _selectTopic, );
+	}
+	
+	void _selectTopic() async {
+		_topic = await Navigate.instance.selectTopic(context);
+	}
+	
+	
+	
 }
