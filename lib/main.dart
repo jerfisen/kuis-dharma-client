fca88fe61ffe8e2155182901eed588102502bfa1
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-16 11:27:57 +0700

    add graphql provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kdygd/Navigate.dart';
+import 'package:kdygd/common/Config.dart';
+import 'package:kdygd/common/LanguageBloc.dart';
 import 'package:kdygd/common/Session.dart';
 import 'package:kdygd/generated/i18n.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
 	Crashlytics.instance.enableInDevMode = true;
@@ -20,29 +24,55 @@ class KDYGD extends StatelessWidget {
 	// This widget is the root of your application.
 	@override
 	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'Flutter Demo',
-			navigatorKey: _navigator_state,
-			localizationsDelegates: [
-				S.delegate,
-				GlobalWidgetsLocalizations.delegate,
-				GlobalMaterialLocalizations.delegate,
-			],
-			supportedLocales: S.delegate.supportedLocales,
-			localeResolutionCallback: S.delegate.resolution( fallback: const Locale('in', 'ID') ),
-			theme: ThemeData(
-				// This is the theme of your application.
-				//
-				// Try running your application with "flutter run". You'll see the
-				// application has a blue toolbar. Then, without quitting the app, try
-				// changing the primarySwatch below to Colors.green and then invoke
-				// "hot reload" (press "r" in the console where you ran "flutter run",
-				// or simply save your changes to "hot reload" in a Flutter IDE).
-				// Notice that the counter didn't reset back to zero; the application
-				// is not restarted.
-				primarySwatch: Colors.blue,
+		final HttpLink http_link = new HttpLink(
+			uri: "$DEFAULT_SERVER_URL/graphql",
+		);
+		final AuthLink auth_link = new AuthLink(
+			getToken: () async {
+				final user = await FirebaseAuth.instance.currentUser();
+				if ( user == null ) return null;
+				else return "bearer ${ await user.getIdToken() }";
+			}
+		);
+		ValueNotifier<GraphQLClient> client = new ValueNotifier(
+			new GraphQLClient(
+				cache: InMemoryCache(),
+				link: auth_link.concat( http_link as Link ),
 			),
-			onGenerateRoute: Navigate.instance.generator,
+		);
+		return new GraphQLProvider(
+			client: client,
+			child: new StreamBuilder<Locale>(
+				stream: Language.instance.locale_stream,
+				builder: ( _, snapshot ) => MaterialApp(
+					title: 'Flutter Demo',
+					navigatorKey: _navigator_state,
+					localizationsDelegates: [
+						S.delegate,
+						GlobalWidgetsLocalizations.delegate,
+						GlobalMaterialLocalizations.delegate,
+					],
+					supportedLocales: S.delegate.supportedLocales,
+					localeResolutionCallback: S.delegate.resolution( fallback: const Locale('in', 'ID') ),
+					locale: snapshot.data ?? const Locale('in', 'ID'),
+					theme: _getThemeData(),
+					onGenerateRoute: Navigate.instance.generator,
+				),
+			),
 		);
 	}
+	
+	ThemeData _getThemeData() => new ThemeData(
+		// This is the theme of your application.
+		//
+		// Try running your application with "flutter run". You'll see the
+		// application has a blue toolbar. Then, without quitting the app, try
+		// changing the primarySwatch below to Colors.green and then invoke
+		// "hot reload" (press "r" in the console where you ran "flutter run",
+		// or simply save your changes to "hot reload" in a Flutter IDE).
+		// Notice that the counter didn't reset back to zero; the application
+		// is not restarted.
+		primarySwatch: Colors.blue,
+	);
+	
 }
