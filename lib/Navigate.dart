24396177708beb2f8381bd7ce81fc16c06c3391f
leoncolt <lionmanbloodsucker@yahoo.com>
2019-06-26 11:48:58 +0700

    add create and view article

--- lib/Navigate.dart
+++ lib/Navigate.dart
@@ -1,10 +1,14 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:kdygd/model/Article.dart';
 import 'package:kdygd/model/Question.dart';
+import 'package:kdygd/model/Topic.dart';
 import 'package:kdygd/view/MainPage.dart';
 import 'package:kdygd/view/SplashScreen.dart';
 import 'package:kdygd/view/exam/DoExam.dart';
+import 'package:kdygd/view/article/ArticleEditor.dart';
+import 'package:kdygd/view/article/ArticlePage.dart';
 import 'package:kdygd/view/topic/SelectTopicPage.dart';
 
 class Navigate {
@@ -36,11 +40,28 @@ class Navigate {
 				handlerFunc: ( _, __ ) => new SelectTopicPage(),
 			),
 		);
+		_router.define(
+			'topic/:topic/article/create',
+			handler: new Handler(
+				handlerFunc: ( final BuildContext context, final Map<String, dynamic> params ) => new ArticleEditor(
+					article: new Article(
+						topic_id: params["topic"][0],
+						content: null,
+					),
+				),
+			),
+		);
 	}
 	
 	Future<void> home( final BuildContext context, { final bool clear_stack = false, final bool replace = false } ) => _router.navigateTo(context, '/home', clearStack: clear_stack, replace: replace);
 	Future selectTopic( final BuildContext context ) => _router.navigateTo(context, '/topic/select');
 	Future<void> doExam( final BuildContext context, { @required final List<Question> questions, @required final Duration start_duration } ) => Navigator.of(context).push( new MaterialPageRoute(
 		builder: ( _ ) => new DoExamPage(questions: questions, start_duration: start_duration,),
+	) );
+	Future<void> createArticle( final BuildContext context, Topic topic ) => _router.navigateTo(context, 'topic/${topic.id}/article/create');
+	Future<void> viewArticle( final BuildContext context, Article article ) => Navigator.of(context).push( new MaterialPageRoute(
+		builder: ( _ ) => new ArticlePage(
+			article: article,
+		),
 	) );
 }
